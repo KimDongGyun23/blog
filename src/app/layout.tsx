8f858d475942183f51dcb4566f6b450917563f91
DongGyun <wooheuy5752@gmail.com>
2025-01-29 08:23:58 +0900

    rename: SideNavigation 파일 위치 변경

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,9 +2,8 @@ import type { Metadata } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
 import Link from 'next/link'
 
-import { SideNavigation } from './(home)/_components/SideNavigation'
-
 import '@/styles/index.css'
+import { SideNavigation } from '@/components/SideNavigation'
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
